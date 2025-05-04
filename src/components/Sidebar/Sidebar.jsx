944c9b4b751878b8bf0652d3139bad557199b28f
Camilla <camillap@id.uff.br>
2025-05-04 14:49:30 -0300

    adicionar responsividade e menu hamburguer

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -3,6 +3,7 @@ import SidebarFooter from "./SidebarFooter";
 import SidebarHeader from "./SidebarHeader";
 import DropdownMenu from "../DropdownMenu";
 import { Link, useNavigate } from "react-router-dom";
+import { useState } from "react";
 
 const Sidebar = () => {
   const cursosGraduacao = [
@@ -11,22 +12,34 @@ const Sidebar = () => {
     <Link to="">Ciência da Computação (Rio das Ostras)</Link>,
     <Link to="">Tecnologia em Sistemas de Computação</Link>,
   ];
+
   const cursosPos = [
     <Link to="/mestrado">Mestrado</Link>,
     <Link to="/doutorado">Doutorado</Link>,
   ];
+
   const navigate = useNavigate();
+  const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <div className="sidebar-container">
-      <SidebarHeader />
-      <div className="sidebar-menus sidebar">
-        <button onClick={() => navigate("/")}>Home</button>
-        <DropdownMenu title="Graduação" items={cursosGraduacao} />
-        <DropdownMenu title="Pós Graduação" items={cursosPos} />
+    <>
+      <button onClick={() => setIsOpen(!isOpen)} className="hamburger-button">
+        ☰
+      </button>
+      <div
+        className={`sidebar-container responsive-sidebar ${
+          isOpen ? "open" : ""
+        }`}
+      >
+        <SidebarHeader />
+        <div className="sidebar-menus sidebar">
+          <button onClick={() => navigate("/")}>Home</button>
+          <DropdownMenu title="Graduação" items={cursosGraduacao} />
+          <DropdownMenu title="Pós Graduação" items={cursosPos} />
+        </div>
+        <SidebarFooter />
       </div>
-      <SidebarFooter />
-    </div>
+    </>
   );
 };
 
