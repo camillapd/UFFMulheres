d2a9f1f3f9e0984a5d93fec93996f0818bba9f30
Camilla <camillap@id.uff.br>
2025-05-04 12:46:56 -0300

    mudar link do home

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -2,7 +2,7 @@ import "./Sidebar.css";
 import SidebarFooter from "./SidebarFooter";
 import SidebarHeader from "./SidebarHeader";
 import DropdownMenu from "../DropdownMenu";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 const Sidebar = () => {
   const cursosGraduacao = [
@@ -15,11 +15,13 @@ const Sidebar = () => {
     <Link to="/mestrado">Mestrado</Link>,
     <Link to="/doutorado">Doutorado</Link>,
   ];
+  const navigate = useNavigate();
 
   return (
     <div className="sidebar-container">
       <SidebarHeader />
       <div className="sidebar-menus sidebar">
+        <button onClick={() => navigate("/")}>Home</button>
         <DropdownMenu title="Graduação" items={cursosGraduacao} />
         <DropdownMenu title="Pós Graduação" items={cursosPos} />
       </div>
--- src/components/Sidebar/SidebarHeader.jsx
+++ src/components/Sidebar/SidebarHeader.jsx
@@ -1,14 +1,11 @@
 import "./SidebarHeader.css";
 import logo from "../../assets/uff_logo.png";
-import { Link } from "react-router-dom";
 
 const SidebarHeader = () => {
   return (
     <div>
       <h2 className="sidebarheader-title">
-        <Link to="/">
-          <img src={logo} alt="UFF logo" /> Mulheres
-        </Link>
+        <img src={logo} alt="UFF logo" /> Mulheres
       </h2>
     </div>
   );
