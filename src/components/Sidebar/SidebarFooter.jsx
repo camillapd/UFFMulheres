b9092bb521b303df0a72de46683c9c029412f9ab
Camilla <camillap@id.uff.br>
2025-05-04 14:50:36 -0300

    adicionar footer

--- src/components/Sidebar/SidebarFooter.jsx
+++ src/components/Sidebar/SidebarFooter.jsx
@@ -1,5 +1,23 @@
+import "./SidebarFooter.css";
+
 const SidebarFooter = () => {
-  return <div>Informações do footer</div>;
+  return (
+    <footer className="footer">
+      <p>© 2025 Universidade Federal Fluminense — Projeto de Conclusão de Curso</p>
+
+      <div className="footer-links">
+        <a
+          href="https://github.com/camillapd/UFFMulheres"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Documentação / GitHub
+        </a>
+      </div>
+
+      <p className="footer-version">Versão 1.0 - Projeto Final II</p>
+    </footer>
+  );
 };
 
 export default SidebarFooter;
