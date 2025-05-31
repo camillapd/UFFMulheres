b8191800098415773480848b3020972462ed8ba5
Camilla <camillap@id.uff.br>
2025-05-31 16:25:22 -0300

    add dashboard doutorado

--- src/pages/DashboardDoutorado.jsx
+++ src/pages/DashboardDoutorado.jsx
@@ -1,8 +1,145 @@
+import DashboardLayout from "../components/DashboardLayout";
+import ChartCard from "../components/ChartCard";
+import BarChart from "../components/Charts/BarChart";
+import InfoCard from "../components/InfoCard";
+import Glossary from "../components/Glossary";
+import { glossaryPos } from "../glossaries/glossaryPos";
+import { formatGlossary } from "../utils/formatGlossary";
 
 export default function DashboardDoutorado() {
-    return (
-      <div>
-       
-      </div>
-    )
-  }
+  const charts = (
+    <>
+      <ChartCard
+        title="Alunos ativos por tipo de matrícula"
+        ChartComponent={BarChart}
+        chartProps={{
+          csvFileName: "doutorado/doutorado_tipo_matricula.csv",
+          xColumn: "Tipo",
+          valueColumns: ["Feminino", "Masculino"],
+          groupMode: "grouped",
+          legendOffsetBt: 35,
+          ariaLabel:
+            "Gráfico de barras mostrando a quantidade de alunos ativos no doutorado",
+        }}
+      />
+
+      <ChartCard
+        title="Alunos ativos que pediram prorrogação"
+        ChartComponent={BarChart}
+        chartProps={{
+          csvFileName: "doutorado/doutorado_prorrogacao.csv",
+          xColumn: "Razão",
+          valueColumns: ["Feminino", "Masculino"],
+          groupMode: "grouped",
+          legendOffsetBt: 35,
+          ariaLabel:
+            "Gráfico de barras mostrando a quantidade de alunos que pediram prorrogação no doutorado",
+        }}
+      />
+
+      <ChartCard
+        title="Alunos que foram desligados da UFF por razão"
+        ChartComponent={BarChart}
+        chartProps={{
+          csvFileName: "doutorado/doutorado_desligados.csv",
+          xColumn: "Razão",
+          valueColumns: ["Feminino", "Masculino"],
+          groupMode: "grouped",
+          legendOffsetBt: 35,
+          ariaLabel:
+            "Gráfico de barras mostrando a quantidade de alunos do doutorado que foram desligados da UFF, por razão",
+        }}
+      />
+
+      <ChartCard
+        title="Alunos inscritos por ano"
+        ChartComponent={BarChart}
+        chartProps={{
+          csvFileName: "doutorado/doutorado_inscritos_ano.csv",
+          xColumn: "Ano",
+          valueColumns: ["Feminino", "Masculino"],
+          tickRotation: -90,
+          legendOffset: 50,
+          ariaLabel:
+            "Gráfico de barras mostrando a quantidade de alunos inscritos por ano no doutorado",
+        }}
+      />
+
+      <ChartCard
+        title="Alunos formados por ano"
+        ChartComponent={BarChart}
+        chartProps={{
+          csvFileName: "doutorado/doutorado_formados_ano.csv",
+          xColumn: "Ano",
+          valueColumns: ["Feminino", "Masculino"],
+          tickRotation: -90,
+          legendOffset: 50,
+          ariaLabel:
+            "Gráfico de barras mostrando a quantidade de alunos formados por ano no doutorado",
+        }}
+      />
+
+      <ChartCard
+        title="Alunos desligados por rendimento por ano"
+        ChartComponent={BarChart}
+        chartProps={{
+          csvFileName: "doutorado/doutorado_rendimento_ano.csv",
+          xColumn: "Ano",
+          valueColumns: ["Feminino", "Masculino"],
+          tickRotation: -90,
+          ariaLabel:
+            "Gráfico de barras mostrando a quantidade de alunos desligados, por rendimento insuficiente, por ano no doutorado",
+        }}
+      />
+
+      <ChartCard
+        title="Alunos desistentes por ano"
+        ChartComponent={BarChart}
+        chartProps={{
+          csvFileName: "doutorado/doutorado_desistentes_ano.csv",
+          xColumn: "Ano",
+          valueColumns: ["Feminino", "Masculino"],
+          tickRotation: -90,
+          ariaLabel:
+            "Gráfico de barras mostrando a quantidade de alunos desistentes por ano no doutorado",
+        }}
+      />
+
+      <ChartCard
+        title="Alunos trancados por ano"
+        ChartComponent={BarChart}
+        chartProps={{
+          csvFileName: "doutorado/doutorado_trancados_ano.csv",
+          xColumn: "Ano",
+          valueColumns: ["Feminino", "Masculino"],
+          tickRotation: -90,
+          ariaLabel:
+            "Gráfico de barras mostrando a quantidade de alunos que pediram trancamento, por ano no doutorado",
+        }}
+      />
+    </>
+  );
+
+  const cards = (
+    <>
+      <InfoCard title="Total de alunos Ativos" value={63} />
+      <InfoCard title="Alunas Ativas" value={16} />
+      <InfoCard title="Alunos Ativos" value={47} />
+    </>
+  );
+
+  const glossary = (
+    <>
+      <Glossary content={formatGlossary(glossaryPos)} />
+    </>
+  );
+
+  return (
+    <DashboardLayout
+      titulo="Dashboard do Doutorado"
+      cards={cards}
+      charts={charts}
+      glossary={glossary}
+    ></DashboardLayout>
+  );
+}
