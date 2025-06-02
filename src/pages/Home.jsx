94e9a23916ee0c8f0458a66f69e7055a28a960e9
Camilla <camillap@id.uff.br>
2025-06-02 18:08:51 -0300

    homepage wip

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,12 +3,13 @@ import ChartCard from "../components/ChartCard";
 import InfoCard from "../components/InfoCard";
 import PieChart from "../components/Charts/PieChart";
 import BarChart from "../components/Charts/BarChart";
+import RadarChart from "../components/Charts/RadarChart";
 
 export default function Home() {
   const charts = (
     <>
       <ChartCard
-        title="Total de alunas da Graduação"
+        title="Total de alunas ativas da Graduação por curso"
         ChartComponent={PieChart}
         chartProps={{
           csvFileName: "graduacao/total_alunas.csv",
@@ -16,18 +17,63 @@ export default function Home() {
           valueColumns: ["Total"],
         }}
       />
+      <ChartCard
+        title="Total de alunos ativos da Graduação por curso"
+        ChartComponent={RadarChart}
+        chartProps={{
+          csvFileName: "graduacao/alunos_ativos_geral.csv",
+          xColumn: "Curso",
+          valueColumns: ["Feminino", "Masculino"],
+        }}
+      />
+
+      <ChartCard
+        title="Total de alunas ativas da Pós Graduação por curso"
+        ChartComponent={PieChart}
+        chartProps={{
+          csvFileName: "posgrad/total_alunas.csv",
+          xColumn: "Curso",
+          valueColumns: ["Total"],
+        }}
+      />
+      <ChartCard
+        title="Alunos com formação em Graduação"
+        ChartComponent={BarChart}
+        chartProps={{
+          csvFileName: "posgrad/formacao_em_graduacao.csv",
+          xColumn: "Curso",
+          valueColumns: ["Feminino", "Masculino"],
+          layout: "horizontal",
+          legendOffsetBt: 35,
+          legendOffsetLeft: -220,
+          tickTextAnchor: "end",
+          marginLeft: 230,
+        }}
+      />
+      <ChartCard
+        title="Alunos com formação em Mestrado"
+        ChartComponent={BarChart}
+        chartProps={{
+          csvFileName: "posgrad/formacao_em_mestrado.csv",
+          xColumn: "Curso",
+          valueColumns: ["Feminino", "Masculino"],
+        }}
+      />
     </>
   );
 
   const cards = (
     <>
-      <InfoCard title="Total de alunos Ativos da Graduação" value={6449} />
-      <InfoCard title="Alunas Ativas" value={1157} />
-      <InfoCard title="Alunos Ativos" value={5292} />
-
-      <InfoCard title="Total de alunos Ativos da Pós Graduação" value={232} />
-      <InfoCard title="Alunas Ativas" value={43} />
-      <InfoCard title="Alunos Ativos" value={189} />
+      <div>
+        <InfoCard title="Total de alunos Ativos da Graduação" value={6449} />
+        <InfoCard title="Alunas Ativas" value={1157} />
+        <InfoCard title="Alunos Ativos" value={5292} />
+      </div>
+      <div>
+        <InfoCard title="Total de alunos Ativos da Pós Graduação" value={232} />
+        <InfoCard title="Alunas Ativas" value={43} />
+        <InfoCard title="Alunos Ativos" value={189} />
+      </div>
     </>
   );
 
