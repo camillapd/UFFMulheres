88bea642a829403deaff65697fe658d7bd1f23ec
Camilla <camillap@id.uff.br>
2025-06-19 19:55:01 -0300

    add significados nos glossarios

--- src/glossaries/glossaryEad.js
+++ src/glossaries/glossaryEad.js
@@ -1,42 +1,38 @@
 export const glossaryEad = [
   {
-    term: "Aluno Ativo",
-    meaning: ".",
-  },
-  {
     term: "Aluno Trancado",
-    meaning: ".",
+    meaning: "Aluno com matrícula trancada.",
   },
   {
-    term: "Aluno Trancamento Automático",
-    meaning: ".",
+    term: "Aluno com Trancamento Automático",
+    meaning: "Aluno não se inscreveu em nenhuma disciplina no período e não solicitou trancamento.",
   },
   {
-    term: "Aluno Trancamento Especial",
-    meaning: ".",
+    term: "Aluno com Trancamento Especial",
+    meaning: "Aluno solicita via processo por questões previstas no regulamento de cursos. este trancamento não é considerado para o limite de trancamentos a que o aluno tem direito.",
   },
   {
     term: "Aluno Aguardando Atualização",
+    meaning: "Aluno do CEDERJ que finalizou o curriculo e está aguardando os procedimentos da UFF para formatura",
+  },
+  {
+    term: "Ensino a Distância",
     meaning: ".",
   },
   {
-    term: "EAD",
+    term: "Ensino a Distância: Transferência ex officio",
     meaning: ".",
   },
   {
-    term: "EAD:Transferência ex officio",
+    term: "Ensino a Distância: Vestibular",
+    meaning: "Aluno inscrito por meio do Vestibular do CEDERJ.",
+  },
+  {
+    term: "Ensino a Distância: Reingresso",
     meaning: ".",
   },
   {
-    term: "EAD:Vestibular",
-    meaning: ".",
-  },
-  {
-    term: "EAD:Reingresso",
-    meaning: ".",
-  },
-  {
-    term: "EAD:Enem",
-    meaning: ".",
+    term: "Ensino a Distância: Enem",
+    meaning: "Aluno inscrito usando a nota obtida no último ENEM realizado.",
   },
 ];
--- src/glossaries/glossaryGrad.js
+++ src/glossaries/glossaryGrad.js
@@ -1,19 +1,19 @@
 export const glossaryGrad = [
   {
     term: "Aluno Ativo",
-    meaning: ".",
+    meaning: "Alunos que efetuaram a matrícula na universidade em curso de graduação, estando associado a uma das situações definidas pela PROGRAD e DAE junto ao sistema acadêmico como: abandono; afastamento mobilidade internacional; afastamento mobilidade nacional; afastado; excedente em disciplina; formando; inscrito; inscrito sem aprovação; interno; interno com disciplina; pendente; trancado; trancado automático; trancado automático por abandono; e trancamento especial.",
   },
   {
     term: "Aluno Inscrito",
-    meaning: ".",
+    meaning: "Aluno inscrito em disciplinas.",
   },
   {
     term: "Aluno Pendente",
-    meaning: ".",
+    meaning: "Situação intermediária e temporária enquanto não é registrada a situação final do aluno para o semestre.",
   },
   {
     term: "Aluno Concluinte",
-    meaning: ".",
+    meaning: "Aluno que foi indicado pela coordenação como cursando o último período.",
   },
   {
     term: "Aluno Desistente",
@@ -21,46 +21,56 @@ export const glossaryGrad = [
   },
   {
     term: "Transferência interinstucional",
-    meaning: ".",
+    meaning:
+      "Aluno inscrito por Transferência interinstitucional: o ingresso destinado aos estudantes regularmente matriculados em instituição de ensino superior pública, que tenham ingressado por concurso público, mediante requerimento administrativo, condicionada à existência de vagas e obedecidos os critérios estabelecidos pela legislação superior e pelo Cepex/UFF.",
   },
   {
     term: "Transferência facultativa",
-    meaning: ".",
+    meaning:
+      "Aluno inscrito por Transferência facultativa: a vinculação à UFF de discente regularmente matriculado em outra Instituição de Ensino Superior, mediante aprovação e classificação em concurso público e análise documental do cumprimento dos requisitos estabelecidos pelo Colegiado do Curso, obedecidos os critérios definidos pela legislação pertinente e pelo Conselho de Ensino, Pesquisa e Extensão.",
   },
   {
-    term: "Transferência ex officio",
-    meaning: ".",
+    term: "Transferência ex-officio",
+    meaning:
+      "Aluno inscrito por Transferência obrigatória: o ingresso destinado ao estudante regularmente matriculado em instituição de ensino superior pública, que atua ou é dependente de servidor público federal ou membro das Forças Armadas.",
   },
   {
     term: "Reinvinculação cursos afim",
-    meaning: ".",
+    meaning:
+      "Aluno inscrito por Reinvinculação cursos afim: acultado ao discente que desejar ingressar em outro curso de graduação, devendo ser requerido no último período letivo, imediatamente anterior a sua formatura, ficando seu novo ingresso condicionado à existência de vaga e a critérios estabelecidos pelo Colegiado do Curso de destino.",
   },
   {
     term: "Rematrícula",
-    meaning: ".",
+    meaning:
+      "Aluno inscrito por Rematrícula: a concessão de uma nova matrícula ao estudante que teve a matrícula cancelada, observando as condições previstas pelo Regulamento dos Cursos de Graduação em vigor.",
   },
   {
     term: "Reingresso UFF",
-    meaning: ".",
+    meaning: "Aluno inscrito por Reingresso UFF ou Permanência de Vínculo, facultada ao discente que desejar ingressar em uma nova habilitação ou ênfase, do mesmo curso, devendo ser requerida no último período letivo, imediatamente anterior a sua formatura, ficando o seu novo ingresso condicionado à existência de vaga e a critérios estabelecidos pelo seu Colegiado de Curso.",
   },
   {
     term: "Reingresso por concurso público",
-    meaning: ".",
+    meaning:
+      "Aluno inscrito por Reingresso por concurso público: a vinculação à UFF permitida aos portadores de diploma de curso de graduação devidamente reconhecido, oriundos desta ou de outra Instituição de Ensino Superior, mediante aprovação e classificação em concurso público e análise documental do cumprimento dos requisitos estabelecidos pelo Colegiado do Curso, obedecidos os critérios definidos pela legislação pertinente e pelo Conselho de Ensino, Pesquisa e Extensão – CEPEx da Universidade Federal Fluminense.",
   },
   {
     term: "Mudança de curso",
-    meaning: ".",
+    meaning:
+      "Aluno inscrito por Mudança de curso: a vinculação que permite ao discente de um determinado curso de graduação da UFF ingressar em outro curso de graduação, mediante aprovação e classificação em concurso público e análise documental do cumprimento dos requisitos estabelecidos pelo Colegiado do Curso, obedecidos os critérios definidos pela legislação pertinente e pelo Conselho de Ensino, Pesquisa e Extensão – CEPEx da Universidade Federal Fluminense.",
   },
   {
     term: "Mudança de localidade",
-    meaning: ".",
+    meaning:
+      "Aluno inscrito por Mudança de localidade: a vinculação que permite ao discente de determinado curso de graduação da UFF ingressar em outro curso de graduação com a mesma denominação, porém ministrado em outra localidade, por meio de processo seletivo sem aplicação de prova.",
   },
   {
     term: "Convênio cultural/PEC-G",
-    meaning: ".",
+    meaning:
+      "Aluno inscrito pelo PEC-G: a matrícula de estudante estrangeiro na UFF, por meio de convênio cultural firmado entre o Brasil e países conveniados, somente é aceita dentro do número de vagas oferecidas anualmente pela Universidade à Secretaria de Educação Superior (SESu) do Ministério da Educação (MEC), ou Convênio cultural: o ingresso de estudantes estrangeiros com base em acordos culturais firmados entre o Brasil e outros países, bem como entre a UFF e universidades estrangeiras, sujeito à existência de vagas.",
   },
   {
     term: "Mandado de segurança",
-    meaning: ".",
+    meaning:
+      'Aluno inscrito por um Madando de segurança. Segundo o portal do Tribunal de Justiça do Distrito Federal e dos Territórios: "O mandado de segurança é um instrumento jurídico, cuja finalidade é proteger direito líquido e certo, ou seja, provado por documentos, que tenha sido violado por ato ilegal ou abusivo de autoridade pública ou de agente de pessoa jurídica no exercício de atribuições do Poder Público.".',
   },
 ];
